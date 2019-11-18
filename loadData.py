3e4ea0bb0fdfc9814795ccf81e952777082c4793
msk <msk@Shineui-MacBook-Pro.local>
2019-11-17 22:02:10 -0700

    commit

--- loadData.py
+++ loadData.py
@@ -8,71 +8,73 @@ import itertools
 
 GeoIdMatch = ".*<geoID>\s*(\S+)\s*</geoID>.*"
 
-def readTextAndGeoIdSpans(annFiles):
+def readTextAndGeoIdSpansFile(annFile):
+    txtFile = annFile.replace('.ann', '.txt')
 
-    for annFile in annFiles:
-        txtFile = annFile.replace('.ann', '.txt')
+    with open(txtFile, 'rb') as file:
+            text = file.read().decode("utf-8", "surrogatepass")
 
-        with open(txtFile, 'rb') as file:
-                text = file.read().decode("utf-8", "surrogatepass")
+    annIdToSpan = {}
+    spanToGeoID = {}
 
-        annIdToSpan = {}
-        spanToGeoID = {}
+    with open(annFile, 'r') as file:
+        annText = file.read()
+        annText = re.sub(r"\n([^T#])", r" ", annText)
+        lines = re.split('\r|\n', annText)
+        pos = 0
 
-        with open(annFile, 'r') as file:
-            annText = file.read()
-            annText = re.sub(r"\n([^T#])", r" ", annText)
-            lines = re.split('\r|\n', annText)
-            pos = 0
+        for line in lines:
+            line = line.strip()
+            row = line.split('\t')
+            if len(row) > 0:
+                if row[0].startswith('T'):
+                    if row[1].startswith('Location'):
+                        position = row[1].replace(';', ' ').split()
+                        startP = int(position[1])
+                        endP = int(position[-1])
 
-            for line in lines:
-                line = line.strip()
-                row = line.split('\t')
-                if len(row) > 0:
-                    if row[0].startswith('T'):
-                        if row[1].startswith('Location'):
-                            position = row[1].replace(';', ' ').split()
-                            startP = int(position[1])
-                            endP = int(position[-1])
+                        locationText = text[startP:endP]
 
-                            locationText = text[startP:endP]
+                        if re.sub(r'\s+', '', locationText) != re.sub(r'\s+', '', row[2]):
+                            print("WARNING: {0}({1}) from .txt != {2}({3}:{4}) from {5}".format(
+                                list(locationText), str(text).find(row[2], pos+1), row[2], startP, endP, annFile[-14:]))
 
-                            if re.sub(r'\s+', '', locationText) != re.sub(r'\s+', '', row[2]):
-                                print("WARNING: {0}({1}) from .txt != {2}({3}:{4}) from {5}".format(
-                                    list(locationText), str(text).find(row[2], pos+1), row[2], startP, endP, annFile[-14:]))
+                        pos = endP
+                        span = (startP, endP)
+                        annIdToSpan[row[0]] = span
 
-                            pos = endP
-                            span = (startP, endP)
-                            annIdToSpan[row[0]] = span
+                    elif row[1].startswith('Protein'):
+                        pass
 
-                        elif row[1].startswith('Protein'):
-                            pass
+                elif row[0].startswith('#'):
+                    noteId = row[1].split()[1]
 
-                    elif row[0].startswith('#'):
-                        noteId = row[1].split()[1]
-
-                        # if noteId in annIdToSpan:
-                        match = re.search(GeoIdMatch, row[2])
-                        if match:
-                            geoId = match.groups()[0].strip()
-                            spanToGeoID[noteId] = geoId
-                        # else:
-                        # 	print(noteId)
+                    # if noteId in annIdToSpan:
+                    match = re.search(GeoIdMatch, row[2])
+                    if match:
+                        geoId = match.groups()[0].strip()
+                        spanToGeoID[noteId] = geoId
+                    # else:
+                    #   print(noteId)
 
 
-        spans = sorted(annIdToSpan.items(), key=operator.itemgetter(1))
-        geoIds = sorted(spanToGeoID.items(), key=lambda i:[j[0] for j in spans].index(i[0]))
+    spans = sorted(annIdToSpan.items(), key=operator.itemgetter(1))
+    geoIds = sorted(spanToGeoID.items(), key=lambda i:[j[0] for j in spans].index(i[0]))
 
-        # try:
-        # 	geoIds = sorted(spanToGeoID.items(), key=lambda i:[j[0] for j in spans].index(i[0]))
-        # # print(geoIds)
-        # except:
-        # 	print("error in {0}".format(annFile))
-            # exit(0)
-
-        yield text, [i[1] for i in spans], [i[1] for i in geoIds]
+    # try:
+    #   geoIds = sorted(spanToGeoID.items(), key=lambda i:[j[0] for j in spans].index(i[0]))
+    # # print(geoIds)
+    # except:
+    #   print("error in {0}".format(annFile))
         # exit(0)
-        
+
+    return text, [i[1] for i in spans], [i[1] for i in geoIds]
+
+
+def readTextAndGeoIdSpans(annFiles):
+    for annFile in annFiles:
+        yield readTextAndGeoIdSpansFile(annFile)
+
 
 def getInputData(tokenizer, normalizer):
     docs = []
@@ -122,6 +124,7 @@ def pad_sequence(l, v, n):
 def chunk_data(data, tokenizer, max_token):
     tot_tokens = []
     tot_labels = []
+    tot_spans = []
 
     for doc in data:
         tokens = list( [tokenizer.convert_tokens_to_ids(txt) for txt in divide_chunks(doc[0], max_token) ])
@@ -133,17 +136,28 @@ def chunk_data(data, tokenizer, max_token):
         
         
         tot_tokens.extend(tokens)
-        tot_labels.extend(labels)        
+        tot_labels.extend(labels)  
+        tot_spans.extend(doc[2])      
 #         yield tokens, labels
-    return tot_tokens, tot_labels
+    return tot_tokens, tot_labels, tot_spans
 
-        
+# From Path
 def getData(path, tokenizer, max_token=512):
     annFiles = glob.glob(path + '/*.ann')
     normalizer = readTextAndGeoIdSpans(annFiles)
     data = getInputData(tokenizer, normalizer)  
 
-    tokens, labels = chunk_data(data, tokenizer, max_token)
+    tokens, labels, spans = chunk_data(data, tokenizer, max_token)
     attention_masks = [[float(i>0) for i in ii] for ii in tokens]
 
-    return tokens, labels, attention_masks
+    return tokens, labels, attention_masks, spans
+
+# From File
+def getDataFile(file, tokenizer, max_token=512):
+    normalizer = [readTextAndGeoIdSpansFile(file)]
+    data = getInputData(tokenizer, normalizer)  
+
+    tokens, labels, spans = chunk_data(data, tokenizer, max_token)
+    attention_masks = [[float(i>0) for i in ii] for ii in tokens]
+
+    return tokens, labels, attention_masks, spans
